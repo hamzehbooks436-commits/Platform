b9bcff807cd1975fae3203ff08e2a71b77731d4f
hamzehbooks436-commits <hamzehbooks436@gmail.com>
2026-09-13 22:34:07 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyBnIrORg_qzZy5VmnlEodiyNd0u2MEBe88",
   authDomain: "platform-f0dd3.firebaseapp.com",
-  databaseURL: "https://console.firebase.google.com/u/0/project/platform-f0dd3/overview?fb_gclid=Cj0KCQjw16_UBhCqARIsAIdOaXxwqGU8K4Q28fXRjG-cA60HyucCGYXoN47LVFHgoQQ5sUFS0VQPuKcaAiyqEALw_wcB&fb_utm_campaign=Cloud-SS-DR-Firebase-FY26-global-gsem-1713590&fb_utm_content=text-ad&fb_utm_medium=cpc&fb_utm_source=google&fb_utm_term=KW_firebase&cloudshell=true",
+  databaseURL: "https://platform-f0dd3-default-rtdb.firebaseio.com",
   projectId: "platform-f0dd3",
   storageBucket: "platform-f0dd3.firebasestorage.app",
   messagingSenderId: "965309259571",
