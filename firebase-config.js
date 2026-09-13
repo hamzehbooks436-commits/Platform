638c119494d9e86c5df126c28c6250e24e603fc5
hamzehbooks436-commits <hamzehbooks436@gmail.com>
2026-09-13 22:52:24 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyB3HCXBnbq1s7HhZ6VHmZwSZT0rRbAu6yU",
   authDomain: "platform-5180c.firebaseapp.com",
-  databaseURL: "https://platform-5180c-default-rtdb.firebaseio.com",
+  databaseURL: "https://platform-5180c-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "platform-5180c",
   storageBucket: "platform-5180c.firebasestorage.app",
   messagingSenderId: "507347468058",
